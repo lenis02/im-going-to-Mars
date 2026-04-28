c8a3b5fce615837c65b55ad41630e37ce3a5baca
Wonyoung Jung <2655362@naver.com>
2026-04-28 14:50:51 +0900

    feat: add TypeORM and PostgreSQL   connection setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('database.host'),
+        port: config.get<number>('database.port'),
+        username: config.get<string>('database.username'),
+        password: config.get<string>('database.password'),
+        database: config.get<string>('database.database'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
+        synchronize: config.get<string>('NODE_ENV') !== 'production',
+        migrationsRun: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
