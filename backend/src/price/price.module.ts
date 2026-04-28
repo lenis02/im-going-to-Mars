d7d19aa01943c8471a7cd81d4725e2e5ca99061b
Wonyoung Jung <2655362@naver.com>
2026-04-28 15:24:38 +0900

    feat: 일봉 OHLCV 가격 데이터 CRUD API 구현

--- backend/src/price/price.module.ts
+++ backend/src/price/price.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DailyPrice } from './entities/daily-price.entity';
 import { PriceController } from './price.controller';
 import { PriceService } from './price.service';
+import { StockModule } from '../stock/stock.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DailyPrice]), StockModule],
   controllers: [PriceController],
   providers: [PriceService],
+  exports: [PriceService],
 })
 export class PriceModule {}
